dbe74393f8adc4fcfefbe327ee3a80a67f842db1
Hugo Leonardo <hugolps@gmail.com>
2022-09-24 12:43:20 -0300

    Prática EJB

---
+++ src/main/java/com/gugawag/pdist/ejbs/MensagemService.java
@@ -0,0 +1,29 @@
+package com.gugawag.pdist.ejbs;
+
+import com.gugawag.pdist.model.Mensagem;
+
+import javax.ejb.EJB;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Stateless(name = "mensagemService")
+@Remote
+public class MensagemService {
+
+    @EJB
+    private MensagemDAO mensagemDAO;
+
+    public List<Mensagem> listar() {
+        return mensagemDAO.listar();
+    }
+
+    public void inserir(long id, String mensagem) {
+        Mensagem novaMensagem = new Mensagem(id, mensagem);
+        mensagemDAO.inserir(novaMensagem);
+    }
+
+    public Mensagem pesquisarPorId(long id) {
+        return mensagemDAO.pesquisarPorId(id);
+    }
+}
